b4680e168d7e774fd4b17ceed5645b94906e85d0
Mohammed Amin <m.amin@delmon.com.sa>
2022-11-06 10:46:45 +0300

    Login Done

--- DelmonERP/Controllers/HomeController.cs
+++ DelmonERP/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace DelmonERP.Controllers
 {
     public class HomeController : Controller
     {
+        DatabaseAcess.CloudErpV1Entities1 db = new DatabaseAcess.CloudErpV1Entities1();
         public ActionResult Index()
         {
             return View();
@@ -17,9 +18,38 @@ namespace DelmonERP.Controllers
         {
             return View();
         }
-        public ActionResult LoginUser(string username,string password)
+
+        [HttpPost]
+        public ActionResult LoginUser(string email,string password)
         {
-            return View();
+            var user = db.tblUsers.Where( u=> u.Email== email && u.Password == password).FirstOrDefault();
+            if (user != null)
+            {
+                Session["UserTypeID"] = user.UserTypeID;
+                Session["FullName"] = user.FullName;
+                Session["Email"] = user.Email;
+                Session["ContactNo"] = user.ContactNo;
+                Session["UserName"] = user.UserName;
+                Session["Password"] = user.Password;
+                Session["IsActive"] = user.IsActive;
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+
+                ViewBag.Message = "Incorrect creditionals";
+                Session["UserTypeID"] = string.Empty;
+                Session["FullName"] = string.Empty;
+                Session["Email"] = string.Empty;
+                Session["ContactNo"] = string.Empty;
+                Session["UserName"] = string.Empty;
+                Session["Password"] = string.Empty;
+                Session["IsActive"] = string.Empty;
+            }
+
+
+            return View("Login");
         }
         public ActionResult ForgetPassword()
         {
