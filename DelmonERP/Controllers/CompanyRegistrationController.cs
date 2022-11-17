6ff4231b1884c8ab26853f3b2d62a0fde9050ff1
Mohammed Amin <m.amin@delmon.com.sa>
2022-11-17 16:00:45 +0300

    Finish Company Registration

---
+++ DelmonERP/Controllers/CompanyRegistrationController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseAcess;
+using DelmonERP.Models;
+
+namespace DelmonERP.Controllers
+{
+    public class CompanyRegistrationController : Controller
+     
+    {
+        CloudErpV1Entities1 db = new CloudErpV1Entities1();
+        // GET: CompanyRegistration
+
+        public ActionResult RegistrationForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegistrationForm(
+            
+            string UserName,
+            string Password,
+            string CPassword,
+            string EName,
+            string EContactNo,
+            string EEmail,
+            string ECNIC,
+            string EDesignation,
+            string EDescription,
+            float  EMonthlySalary,
+            string EAddress,
+            string CName,
+            string BranchName,
+            string  BranchContact,
+            string  BranchAddress
+            )
+        {
+            try
+            {
+                 if (!string.IsNullOrEmpty(UserName)
+                    && !string.IsNullOrEmpty(Password)
+                    && !string.IsNullOrEmpty(CPassword)
+                    && !string.IsNullOrEmpty(EName)
+                    && !string.IsNullOrEmpty(EContactNo)
+                    && !string.IsNullOrEmpty(EEmail)
+                    && !string.IsNullOrEmpty(ECNIC)
+                    && !string.IsNullOrEmpty(EDesignation)
+                    && !string.IsNullOrEmpty(EDescription)
+                    && !string.IsNullOrEmpty(EAddress)
+                    && EMonthlySalary > 0
+                    && !string.IsNullOrEmpty(BranchName)
+                    && !string.IsNullOrEmpty(BranchContact)
+                    && !string.IsNullOrEmpty(BranchAddress)
+                    && !string.IsNullOrEmpty(CName)
+                       )
+                {
+                    var company = new tblCompany()
+                    {
+                        Name = CName ,
+                        Logo = string.Empty
+                    };
+                    db.tblCompanies.Add(company);
+                    db.SaveChanges();
+
+                    var branch = new tblBranch()
+                    {
+                        BranchName = BranchName,
+                        BranchContact = BranchContact,
+                        BranchAddress = BranchAddress,
+                        BranchTypeID = 1,
+                        CompanyID = company.CompanyID,
+                        BrchID = null
+
+                    };
+                    db.tblBranches.Add(branch);
+                    db.SaveChanges();
+
+
+                    var user = new tblUser()
+                    {
+                        ContactNo = EContactNo,
+                        UserName = UserName,
+                        Password = Password,
+                        IsActive = true,
+                        Email = EEmail,
+                        FullName = EName,
+                        UserTypeID = 2
+
+                    };
+                    db.tblUsers.Add(user);
+                    db.SaveChanges();
+
+                    var employee = new tblEmployee()
+                    {
+                        Address = EAddress,
+                        BranchID = branch.BranchID,
+                        CNIC = ECNIC,
+                        CompanyID = company.CompanyID,
+                        ContactNo = EContactNo,
+                        Designation = EDesignation,
+                        Email = EEmail,
+                        MonthlySalary = EMonthlySalary,
+                        UserID = user.UserID,
+                        Name = EName,
+                        Description = string.Empty
+                    };
+                    db.tblEmployees.Add(employee);
+                    db.SaveChanges();
+
+                    ViewBag.Message= "Registration Successfully ";
+                    return RedirectToAction("Login","Home");
+                }
+                else
+                {
+                    ViewBag.Message = "Please Provide Correct Details !";
+                    return View("RegistrationForm");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //     ViewBag.Message = "Please Contact To Administrator !";
+                    ViewBag.Message = ex;
+
+                return View();
+            }
+
+         
+
+        }
+    }
+}
---
+++ DelmonERP/Models/CompanyRegistrationMV.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DelmonERP.Models
+{
+    public class CompanyRegistrationMV
+    {
+        //user details
+        public int UserID { get; set; }
+        public int UserTypeID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool UserStatus { get; set; }
+
+
+        //company details
+        [Required(ErrorMessage = "*Requried")]
+        public string CName { get; set; }
+       
+
+        // Branch Details
+        public string BranchName { get; set; }
+        public string BranchContact { get; set; }
+        public string BranchAddress { get; set; }
+
+
+
+
+        // employee details
+        public int EmployeeID { get; set; }
+        public string EName { get; set; }
+        public string EContactNo { get; set; }
+        public string EEmail { get; set; }
+        public string EAddress { get; set; }
+        public string ECNIC { get; set; }
+        public string EDesignation { get; set; }
+        public string EDescription { get; set; }
+        public double EMonthlySalary { get; set; }
+        
+
+
+    }
+}
